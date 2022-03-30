3d5846b1e7238ee9f9da47342756525a30bec5fc
jake grella <jake@jakegrella.com>
2022-03-30 15:42:57 -0500

    add files + webpack config

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div>
+            <h1>webapp starter</h1>
+            <p>by headed westtt</p>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('app');
+
+// Create *and* render a root with hydration.
+const root = ReactDOMClient.hydrateRoot(container, <App />);
