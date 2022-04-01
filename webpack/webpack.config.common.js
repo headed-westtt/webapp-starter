080efb8acb7a230868408fd24d32e737958e4f97
jake grella <jake@jakegrella.com>
2022-04-01 07:55:46 -0500

    add MiniCssExtractPlugin to prod webpack config

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -26,10 +26,6 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
     ],
   },
   plugins: [
